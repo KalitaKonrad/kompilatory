b24edc81c53265f6a40d26a93e55ccf6ac4439fb
Konrad <konradkalita@student.agh.edu.pl>
2021-06-14 22:01:08 +0200

    feat: add arithmetics and control instructions to AST, add TreePrinter class and two more examples for parses

--- ast.py
+++ ast.py
@@ -1,46 +1,145 @@
-from dataclasses import dataclass
-
-
 class Node:
-    def __init__(self, type, children=None, leaf=None):
-        self.type = type
-        if children:
-            self.children = children
-        else:
-            self.children = []
-        self.leaf = leaf
+    pass
 
-    # instrukcje przypisania,
-    # instrukcje warunkowe if-else,
-    # pętle: while oraz for,
-    # instrukcje break, continue oraz return,
-    # instrukcje print,
-    # instrukcje złożone,
-    # tablice oraz ich zakresy.
 
-@dataclass
-class CompareExpr(Node):
-    op: any
-    left: any
-    right: any
+class Program(Node):
+    def __init__(self, statements_list):
+        self.statements_list = statements_list
 
-@dataclass
+
+class Empty(Node):
+    pass
+
+
+class Number(Node):
+    def __init__(self, number):
+        self.number = number
+
+
+class Expression(Node):
+    def __init__(self, expression):
+        self.expression = expression
+
+
+class InnerVector(Node):
+    def __init__(self, inner_vector, new_expression):
+        self.inner_vector = inner_vector
+        self.new_expression = new_expression
+
+
+class Vector(Node):
+    def __init__(self, inner_vector):
+        self.inner_vector = inner_vector
+
+
+class Matrix(Node):
+    def __init__(self, matrix_type, argument):
+        self.matrix_type = matrix_type
+        self.argument = argument
+
+
+class Range(Node):
+    def __init__(self, from_index, to_index):
+        self.from_index = from_index
+        self.to_index = to_index
+
+
+class UnaryMinus(Node):
+    def __init__(self, value):
+        self.value = value
+
+
 class BinExpr(Node):
-    op: any
-    left: any
-    right: any
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
 
 
-def addToClass(cls):
-    def decorator(func):
-        setattr(cls, func.__name__, func)
-    return decorator
+class MatrixBinExpr(Node):
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
 
-class TreePrinter:
-    ...
-    @addTiClass(BinExpr):
-    def printTree(self.op):
-        print(self.op)
-        self.left.printTree(indent+1)
-        self.left.printTree(indent+2)
-    ...
+
+class Transposition(Node):
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+
+class CompareExpr(Node):
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+
+class SliceArgument(Node):
+    def __init__(self, argument):
+        self.argument = argument
+
+
+class Slice(Node):
+    def __init__(self, identifier, slice_argument_1, slice_argument_2):
+        self.identifier = identifier
+        self.slice_argument_1 = slice_argument_1
+        self.slice_argument_2 = slice_argument_2
+
+
+class SliceOrID(Node):
+    def __init__(self, slice_or_id):
+        self.slice_or_id = slice_or_id
+
+
+class AssignExpr(Node):
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+
+class StatementsList(Node):
+    def __init__(self, statements_list, new_statement):
+        self.statements_list = statements_list
+        self.new_statement = new_statement
+
+
+class Return(Node):
+    def __init__(self, value):
+        self.value = value
+
+
+class CodeBlock(Node):
+    def __init__(self, statements_lists):
+        self.statements_list = statements_lists
+
+
+class LoopStatement(Node):
+    def __init__(self, instruction):
+        self.instruction = instruction
+
+
+class For(Node):
+    def __init__(self, iterator, loop_range, statement):
+        self.iterator = iterator
+        self.loop_range = loop_range
+        self.statement = statement
+
+
+class While(Node):
+    def __init__(self, condition, statement):
+        self.condition = condition
+        self.statement = statement
+
+
+class If(Node):
+    def __init__(self, condition, if_statement, else_statement):
+        self.condition = condition
+        self.if_statement = if_statement
+        self.else_statement = else_statement
+
+
+class Print(Node):
+    def __init__(self, value):
+        self.value = value
---
+++ tree_printer.py
@@ -0,0 +1,226 @@
+from ast import *
+
+
+def add_to_class(cls):
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+    return decorator
+
+
+def print_indent(indent):
+    for i in range(indent):
+        print("|  ", end="")
+
+
+class TreePrinter:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    @add_to_class(Program)
+    def print_tree(self, indent=0):
+        self.statements_list.print_tree(indent)
+
+    @staticmethod
+    @add_to_class(Empty)
+    def print_tree(self, indent=0):
+        pass
+
+    @staticmethod
+    @add_to_class(Number)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.number)
+
+    @staticmethod
+    @add_to_class(Expression)
+    def print_tree(self, indent=0):
+        if isinstance(self.expression, Node):
+            self.expression.print_tree(indent)
+        else:
+            print_indent(indent)
+            print(self.expression)
+
+    @staticmethod
+    @add_to_class(InnerVector)
+    def print_tree(self, indent=0):
+        if self.inner_vector:
+            self.inner_vector.print_tree(indent)
+        self.new_expression.print_tree(indent)
+
+    @staticmethod
+    @add_to_class(Vector)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("VECTOR")
+
+        self.inner_vector.print_tree(indent+1)
+
+    @staticmethod
+    @add_to_class(Matrix)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.matrix_type)
+
+        self.argument.print_tree(indent+1)
+
+    @staticmethod
+    @add_to_class(Range)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("RANGE")
+
+        self.from_index.print_tree(indent + 1)
+        self.to_index.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(UnaryMinus)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("-")
+
+        self.value.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(BinExpr)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.operator)
+
+        self.left.print_tree(indent+1)
+        self.right.print_tree(indent+1)
+
+    @staticmethod
+    @add_to_class(MatrixBinExpr)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.operator)
+
+        self.left.print_tree(indent + 1)
+        self.right.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(Transposition)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("TRANSPOSE")
+
+        self.matrix.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(CompareExpr)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.operator)
+
+        self.left.print_tree(indent + 1)
+        self.right.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(SliceArgument)
+    def print_tree(self, indent=0):
+        self.argument.print_tree(indent)
+
+    @staticmethod
+    @add_to_class(Slice)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("REF")
+
+        print_indent(indent+1)
+        print(self.identifier)
+
+        self.slice_argument_1.print_tree(indent+1)
+        if self.slice_argument_2:
+            self.slice_argument_2.print_tree(indent+1)
+
+    @staticmethod
+    @add_to_class(SliceOrID)
+    def print_tree(self, indent=0):
+        if isinstance(self.slice_or_id, Node):
+            self.slice_or_id.print_tree(indent)
+        else:
+            print_indent(indent)
+            print(self.slice_or_id)
+
+    @staticmethod
+    @add_to_class(AssignExpr)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.operator)
+
+        self.left.print_tree(indent + 1)
+        self.right.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(StatementsList)
+    def print_tree(self, indent=0):
+        if self.statements_list:
+            self.statements_list.print_tree(indent)
+
+        self.new_statement.print_tree(indent)
+
+    @staticmethod
+    @add_to_class(Return)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("RETURN")
+
+        if self.value:
+            self.value.print_tree(indent+1)
+
+    @staticmethod
+    @add_to_class(CodeBlock)
+    def print_tree(self, indent=0):
+        self.statements_list.print_tree(indent)
+
+    @staticmethod
+    @add_to_class(LoopStatement)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print(self.instruction.upper())
+
+    @staticmethod
+    @add_to_class(For)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("FOR")
+
+        print_indent(indent + 1)
+        print(self.iterator)
+
+        self.loop_range.print_tree(indent + 1)
+        self.statement.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(While)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("WHILE")
+
+        self.condition.print_tree(indent + 1)
+        self.statement.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(If)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("IF")
+        self.condition.print_tree(indent + 1)
+
+        print_indent(indent)
+        print("THEN")
+        self.if_statement.print_tree(indent + 1)
+
+        if self.else_statement:
+            print_indent(indent)
+            print("ELSE")
+            self.else_statement.print_tree(indent + 1)
+
+    @staticmethod
+    @add_to_class(Print)
+    def print_tree(self, indent=0):
+        print_indent(indent)
+        print("PRINT")
+
+        self.value.print_tree(indent + 1)
