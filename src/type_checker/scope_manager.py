94c4ef4664535d533278bac623bad7e27f8008cb
Karol Widuch <rolester92@gmail.com>
2021-06-15 17:55:30 +0200

    Add directories, improve node visitor

--- src/type_checker/scope_manager.py
+++ src/type_checker/scope_manager.py
@@ -1,4 +1,4 @@
-from variables_types import Type
+from src.type_checker.variables_types import Type
 
 
 class Symbol:
@@ -21,6 +21,7 @@ class VectorSymbol(Symbol):
     def is_in(self, idx):
         return idx < self.size
 
+
 class MatrixSymbol(Symbol):
     def __init__(self, name, height, width):
         self.name = name
@@ -32,21 +33,27 @@ class MatrixSymbol(Symbol):
         return width_idx < self.width and height_idx < self.height
 
 
-# TODO currently (I think) all we need from here is checking whether we are in the loop
 class SymbolTable(object):
     def __init__(self):
         self.scopes = []
 
     def put(self, name, symbol):  # put variable symbol or fundef under <name> entry
-        self.scopes[-1][1][name] = symbol
+        scope = self._find(name)
+        if scope:
+            scope[name] = symbol
+        else:
+            self.scopes[-1][1][name] = symbol
 
-    def check_exists(self, name):
+    def _find(self, name):
         for _, scope in self.scopes[::-1]:
             if name in scope:
-                return True
-        return False
+                return scope
+        return None
 
-    def get(self, name) -> Symbol:  # get variable symbol or fundef from <name> entry
+    def check_exists(self, name):
+        return self._find(name) is not None
+
+    def get(self, name):  # get variable symbol or fundef from <name> entry
         for _, scope in self.scopes[::-1]:
             if name in scope:
                 return scope[name]
@@ -62,6 +69,13 @@ class SymbolTable(object):
     def is_loop(self):
         for name, _ in self.scopes:
             if name in {"while", "for"}:
+                return True
+
+        return False
+
+    def is_conditional(self):
+        for name, _ in self.scopes:
+            if name in {"while", "if"}:
                 return True
 
         return False
